015deb4b1eaa16a2248492dbe6e278b7651dca77
Daniel Julià <dani@pimpampum.net>
2017-07-03 16:52:48 +0200

    eliminada la opción de declinar

--- pimpampum_cookies.php
+++ pimpampum_cookies.php
@@ -12,7 +12,7 @@ License: GPL2
 function pimpampum_cookies_scripts() {
 
 	wp_enqueue_style( 'cookies',plugins_url( '/cookies/cookietool.css', __FILE__ ), array() );
-	wp_enqueue_script( 'cookies', plugins_url( '/cookies/cookietool.min.js', __FILE__ )  , array(), '1.0.0', true );
+	wp_enqueue_script( 'cookies', plugins_url( '/cookies/cookietool.js', __FILE__ )  , array(), '1.0.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'pimpampum_cookies_scripts',20);
@@ -23,6 +23,9 @@ function cookies_footer() {
 <script>
   CookieTool.Config.set('link', '<?php print bloginfo("url")?>/cookies');
 	CookieTool.Config.set('message','En aquest lloc es fan servir galetes per oferir una experiència més personalitzada. <a href="{{link}}" target="_blank"> Més informació </a>. <br> Ens consent utilitzar galetes? ');
+
+	CookieTool.Config.set('declinetext', '');
+
 	CookieTool.API.ask();
 </script>
 
